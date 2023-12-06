0e4a3c520dadd68073d957ab527aa1ae228bcc05
Ahmed Samy <engahmedeltonamly@gmail.com>
2023-12-06 14:53:50 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 'use strict';
+// Selection List
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //faster
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+//score reset
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+// rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  // 1 generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2 display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3 check for roled 1; if true, switch to next player
+
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    // switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
